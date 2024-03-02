95aa72ea269917dd6e89bb250696f505861b7b98
Rafael Badaro <rafabadaro.15@gmail.com>
2024-03-01 23:33:42 -0300

    creating temp converter

---
+++ metrics-conversion/metrics-conversion/Components/Converters/TemperatureConverterView.swift
@@ -0,0 +1,71 @@
+//
+//  TemperatureConverterView.swift
+//  metrics-conversion
+//
+//  Created by Rafael Badaró on 01/03/24.
+//
+
+import SwiftUI
+
+struct TemperatureConverterView: View {
+    @State private var fahrenheitInput: String = ""
+    @State private var celsiusInput: String = ""
+    @State private var isFahrenheitFieldFocused = false
+    @State private var isCelciusFieldFocused = false
+    
+    
+    
+    var body: some View {
+        
+        VStack {
+            TextField("Enter Fahrenheit", text: $fahrenheitInput, onEditingChanged: setFocusFahrenheit)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onChange(of: fahrenheitInput) { newValue in
+                    
+                    if isFahrenheitFieldFocused {
+                        if let value = Double(newValue) {
+                            celsiusInput = String(format: "%.2f", (value - 32) * 5/9)
+                        } else {
+                            celsiusInput = ""
+                        }
+                    }
+                    
+                }
+            
+            TextField("Enter Celsius", text: $celsiusInput, onEditingChanged: setFocusCelcius)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onChange(of: celsiusInput) { newValue in
+                    
+                    if isCelciusFieldFocused {
+                        if let value = Double(newValue) {
+                            fahrenheitInput = String(format: "%.2f", value * 9/5 + 32)
+                        } else {
+                            fahrenheitInput = ""
+                        }
+                    }
+                }
+        }
+        .padding()
+    }
+    
+    func setFocusFahrenheit(focused: Bool) {
+        isFahrenheitFieldFocused = focused
+    }
+    
+    func setFocusCelcius(focused: Bool) {
+        isCelciusFieldFocused = focused
+    }
+    
+    
+    
+}
+
+struct TemperatureConverterView_Previews: PreviewProvider {
+    static var previews: some View {
+        TemperatureConverterView()
+    }
+}
+
+
---
+++ metrics-conversion/metrics-conversion/metrics_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  metrics_conversionApp.swift
+//  metrics-conversion
+//
+//  Created by Rafael Badaró on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct metrics_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
